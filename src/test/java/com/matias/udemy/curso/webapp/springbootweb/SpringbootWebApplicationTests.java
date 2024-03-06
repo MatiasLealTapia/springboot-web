50007d388b88a27c0df14b3fd01e9955c4207358
MatiasLealTapia <matiasa.lealt.23@gmail.com>
2024-03-05 23:17:12 -0300

    git init

---
+++ src/test/java/com/matias/udemy/curso/webapp/springbootweb/SpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.udemy.curso.webapp.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
