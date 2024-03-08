a6b23c8bfdfaf37bd42d14b164a4d5df8ba90aa5
MatiasLealTapia <matiasa.lealt.23@gmail.com>
2024-03-07 22:12:36 -0300

    Se crea controller con params, ademas se agregan DTO's (Data Transfer Object). Thymeleaf se agrega if, each y listas, controller con Model List<User>

--- src/main/java/com/matias/udemy/curso/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/matias/udemy/curso/webapp/springbootweb/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.matias.udemy.curso.webapp.springbootweb.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,17 +11,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.matias.udemy.curso.webapp.springbootweb.models.User;
+import com.matias.udemy.curso.webapp.springbootweb.models.dto.UserDto;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
     @GetMapping("/details")
-    public Map<String, Object> details() {
+    public UserDto details() {
+        User user = new User("Matías", "Leal");
+
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
+
+        return userDto;
+    }
+
+    @GetMapping("/list")
+    public List<User> list() {
+        User user = new User("Matías", "Leal");
+        User user2 = new User("Andres", "Pizarro");
+        User user3 = new User("Diego", "Zapata");
+
+        List<User> users = Arrays.asList(user, user2, user3);
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
+    }
+
+    @GetMapping("/details-v1")
+    public Map<String, Object> detailsV1() {
+        User user = new User("Matías", "Leal");
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola mundo Spring Boot");
-        body.put("name", "Matias");
-        body.put("lastname", "Leal");
+        body.put("user", user);
         return body;
     }
 
---
+++ src/main/java/com/matias/udemy/curso/webapp/springbootweb/models/dto/ParamDto.java
@@ -0,0 +1,16 @@
+package com.matias.udemy.curso.webapp.springbootweb.models.dto;
+
+public class ParamDto {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}
